9cbf42fd57216bb3276fc34ef916e297558f7c60
ironyman <ironyman@users.noreply.github.com>
2020-01-13 17:46:06 -0800

    Don't use minbase

--- create-vm.sh
+++ create-vm.sh
@@ -36,7 +36,7 @@ chown $original_user:$original_user $KEY $KEY.pub
 MOUNTPOINT=$(mktemp -d)
 
 mount $ROOT $MOUNTPOINT
-debootstrap --include=firefox,openssh-server,xauth --variant=minbase --components=main bionic $MOUNTPOINT
+debootstrap --include=firefox,openssh-server,xauth --components=main bionic $MOUNTPOINT
 
 cp $DIR/$KEY.pub $MOUNTPOINT/
 mkdir -p $MOUNTPOINT/lib/modules/
